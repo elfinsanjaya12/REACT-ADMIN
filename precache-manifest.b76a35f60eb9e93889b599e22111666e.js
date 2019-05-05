4485f6fc31d71f3860b55ed10aff27039e528e3d
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-05-05 14:59:08 +0700

    bismillah

---
+++ precache-manifest.b76a35f60eb9e93889b599e22111666e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/black-dashboard-react/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "c3c31d9533ce98370bfb",
+    "url": "/black-dashboard-react/static/css/main.feb2594b.chunk.css"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/black-dashboard-react/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "636b5951bdce9256be3f",
+    "url": "/black-dashboard-react/static/js/1.636b5951.chunk.js"
+  },
+  {
+    "revision": "b46a1e60e5b4cb2d0205",
+    "url": "/black-dashboard-react/static/js/runtime~main.b46a1e60.js"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/black-dashboard-react/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "c3c31d9533ce98370bfb",
+    "url": "/black-dashboard-react/static/js/main.c3c31d95.chunk.js"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/black-dashboard-react/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/black-dashboard-react/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/black-dashboard-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/black-dashboard-react/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "636b5951bdce9256be3f",
+    "url": "/black-dashboard-react/static/css/1.2ba5163c.chunk.css"
+  },
+  {
+    "revision": "e4ef3813350bbb7e622e70a75964f610",
+    "url": "/black-dashboard-react/index.html"
+  }
+];
